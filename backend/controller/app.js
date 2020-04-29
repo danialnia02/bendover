f3b9d217ae2d2c4edaa1c576748af8abd38680c7
Damenthrall <danial.blackops2@gmail.com>
2020-04-29 15:51:37 +0800

    Hello world

--- backend/controller/app.js
+++ backend/controller/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var snapsell = erquire('../model/snapsell.js');
+var snapsell = require('../model/database.js');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 var formidableMiddleware = require('express-formidable')
@@ -16,9 +16,11 @@ app.get('/test/', function (req, res) {
 
     snapsell.test(function (err, result) {
         if (!err) {
-            res.send(result);
+            res.send("Hello world");
         } else {
             res.status(500).send('Unkown error\nCode:500 Internal Server Error.')
         }
     })
 })
+
+module.exports= app
