2e441f7eb4f4588716203015b0acf5b46949f219
danialnia02 <danial.blackops2@gmail.com>
2020-05-12 17:51:44 +0800

    commiting

--- frontend/frontend/src/pages/dataViewer.js
+++ frontend/frontend/src/pages/dataViewer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './dataViewer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as ReactBootStrap from "react-bootstrap"
+import { Col, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class DataViewer extends React.Component {
   // Select * from database put into code
@@ -43,8 +45,63 @@ class DataViewer extends React.Component {
     //
     return (
       <div className="contain">
-        <div>
-          <div className="container" id="container">
+        <div className="cover">
+          <Col >
+            <th>MusicDb</th>
+          </Col>
+          <div className="container">
+            {/* Header ------------------------------------------ */}
+            <Row>
+
+              <Col xs={2}>
+                <div className="entries_length"><label>Show
+                <select id="entry_length" aria-controls="example"
+                    className="custom-select custom-select-sm form-control-sm">
+                    <option value="5">5</option>
+                    <option value="15">15</option>
+                    <option value="25">25</option>
+                    <option value="30">30</option>
+                  </select> entries
+              </label></div>
+              </Col>
+              <Col sm={1}>
+                <div id="size">Search</div>
+              </Col>
+              <Col sm={2}>
+                <div className="entries_length" id="size">
+                  <select name="attribute" aria-controls="example" className="form-control" id="attribute">
+                    <option value="performanceId">performanceId</option>
+                    <option value="festivalId">festivalId</option>
+                    <option value="startTime">startTime</option>
+                    <option value="endTime">endTime</option>
+                    <option value="popularity">popularity</option>
+                    <option value="dataInserted">dataInserted</option>
+                  </select></div>
+              </Col>
+              <Col sm={2}>
+
+                <div className="entries_length" id="size">
+                  <select name="operation" aria-controls="example" className="form-control" id="operation">
+                    <option value="null">all</option>
+                    <option value="=">==</option>
+                    <option value="=">&gt;=</option>
+                    <option value=">">&gt;</option>
+                    <option value="<">&lt;</option>
+                    <option value="<=">&lt;=</option>
+                  </select></div>
+
+              </Col>
+              <Col>
+
+                {/* cannot put in  input box*/}
+              </Col>
+              <Col sm={2}>
+                <button type="button" id="searchButton">Filter</button>
+              </Col>
+            </Row>
+            {/* ------------------------------------------ */}
+
+
             <ReactBootStrap.Table striped bordered hover>
               <thead>
                 <tr>
