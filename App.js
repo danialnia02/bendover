ba7f7e713252cef229b7701907f4b18db6c0f36a
CleavonOng <64115037+CleavonOng@users.noreply.github.com>
2020-05-14 17:11:11 +0800

    src

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Navbar from './components/Navbar/Navbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
+import Background from './components/Background/Background';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <main style={{marginTop: '120px'}}>
-        <p>This is the page content!</p>
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen}
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false})
+  };
+
+  render() {
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+    return (
+      <div style ={{height:'100%'}}>
+        <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen}/>
+        {backdrop}
+        {/* <main style={{marginTop: '120px',}}> */}
+        <main>
+          <Background />
+            <p>This is the page content!</p>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ components/Background/Background.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import Background from './banner.png';
+import './Background.css';
+
+const pagebackground = props => {
+    return (
+        <div className ="background">
+            <div className ="banner">
+                {/* <img src="./banner.png"/> */}
+            </div>
+        </div>
+      );
+}
+
+export default pagebackground;
