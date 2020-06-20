9f1d6d5450d453b44a92fa9fa78141ba099b07dc
danialnia02 <danial.blackops2@gmail.com>
2020-06-20 22:28:54 +0800

    push

--- frontend/frontend2/src/pages/Home.tsx
+++ frontend/frontend2/src/pages/Home.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import DataViewer from '../components/DataViewer/DataViewer';
+import Works from '../components/DataViewer/works';
 import DataViewer2 from '../components/testFolder/DataViewer2';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonContent>        
-        <DataViewer2/>           
+      <IonContent>
+        <DataViewer />
       </IonContent>
     </IonPage>
   );
